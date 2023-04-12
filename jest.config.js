c6bc14884509ffcd68558f801d9358d00d63f321
Vivek Mishra <vivekhere01@gmail.com>
2023-04-12 06:54:56 +0530

    add UT files

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,28 @@
+const rootDirSource = '<rootDir>/src';
+
 module.exports = {
   preset: 'react-native',
+  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
   transformIgnorePatterns: ['node_modules/?!(static-container)'],
+  cacheDirectory: '.jest/cache',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true,
+    },
+  },
+  clearMocks: true,
+  collectCoverageFrom: [
+    `${rootDirSource}/**/*.{ts,tsx}`,
+    '!**/node_modules/**',
+    `${rootDirSource}/*.constant.ts`,
+    `!${rootDirSource}/**/*.interface.ts`,
+    `!${rootDirSource}/**/*.type.ts`,
+    `!${rootDirSource}/**/index.ts`,
+  ],
 };
