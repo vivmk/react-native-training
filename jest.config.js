70093072544df61a81fd754dc52515b772e54ac4
Vivek Mishra <vivekhere01@gmail.com>
2023-04-12 07:13:44 +0530

    jest files

--- jest.config.js
+++ jest.config.js
@@ -3,13 +3,20 @@ const rootDirSource = '<rootDir>/src';
 module.exports = {
   preset: 'react-native',
   setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  // moduleNameMapper: {
+  //   '^@/(.*)$': '<rootDir>/$1',
+  // },
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/$1',
+    '^[./a-zA-Z0-9@$_-]+\\.(png|jpg)$':
+      '<rootDir>/node_modules/react-native/jest/assetFileTransformer.js',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
   testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
-  transformIgnorePatterns: ['node_modules/?!(static-container)'],
+  // transformIgnorePatterns: ['node_modules/?!(static-container)'],
+  transformIgnorePatterns: [
+    'node_modules/(?!react-native|react-native-gesture-handler|react-navigation|@react-native-async-storage/async-storage|redux-persist|react-native-keychain|)/',
+  ],
   cacheDirectory: '.jest/cache',
   globals: {
     'ts-jest': {
@@ -25,4 +32,11 @@ module.exports = {
     `!${rootDirSource}/**/*.type.ts`,
     `!${rootDirSource}/**/index.ts`,
   ],
+  verbose: true,
+  modulePaths: ['<rootDir>/src/'],
+  setupFiles: [],
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.js$': 'babel-jest',
+  },
 };
